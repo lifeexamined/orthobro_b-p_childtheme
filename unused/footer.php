27dc769344b482f4d5331592b153f1e7a23e584e
lifeexamined <nikita.a.novik@gmail.com>
2021-02-07 16:41:46 -0700

    second commit

---
+++ unused/footer.php
@@ -0,0 +1,15 @@
+<footer>
+    <div class="footer-wrapper">
+        <div class="footer-content">
+            <p>Orthodox Brotherhood of St. Eustace 2021</p>
+        </div>
+    </div>
+</footer>
+<?php 
+
+wp_footer(); 
+
+?>
+</div>
+</body>
+</html>
---
+++ unused/header.php
@@ -0,0 +1,20 @@
+<?php
+
+/* *
+ *
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * */
+
+?>
+
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<?php wp_head(); ?>
+</head>
+<body>
