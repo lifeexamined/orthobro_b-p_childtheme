3d9c9710938da7ed1b41dd997c867a472365034c
lifeexamined <nikita.a.novik@gmail.com>
2021-02-09 12:32:25 -0700

    changed colors and some responsive features

--- front-page.php
+++ front-page.php
@@ -14,6 +14,20 @@
 
 get_header(); 
 ?>
+<div class="post-grid-double-wrapper">
+<?php
+    $args = array( 'numberposts' => '6' );
+	$recent_posts = wp_get_recent_posts( $args );
+	$bgcolor = get_the_post_thumbnail_url(get_the_ID(),'full');
+    foreach( $recent_posts as $recent ){
+        printf( '<div class="post-grid-wrapper"><div style="background:url(\'%3$s\'); background-size: cover" class="post-grid"><a href="%1$s">%2$s</a></div></div>',
+             esc_url( get_permalink( $recent['ID'] ) ),
+			 apply_filters( 'the_title', $recent['post_title'], $recent['ID'] ),
+			 esc_url( get_the_post_thumbnail_url($recent['ID']) )
+         );
+    }
+?>
+</div>
 
 		<main id="main" class="site-main">
 
